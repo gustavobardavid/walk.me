475b6817c012883a2c6cb650ce3f6c7d9baaa17a
WescAlves <wesley.c.alves98@gmail.com>
2024-09-27 13:48:13 -0300

    Finalizando

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -7,6 +7,5 @@ const router = Router();
 const upload = multer({ storage:storage })
 
 router.post('/:id',upload.single("file"), saveImage);
-router.use('/files',express.static('uploads') )
 
 export default router;
